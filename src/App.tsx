d5c0022e6d83fbbb9f497e80d8dca48e731614ee
devtubercode <devtubercode@gmail.com>
2024-10-08 06:03:05 -0500

    feat: delete comments and css file from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import "./App.css";
 import { ApolloClientProvider, ReduxProvider } from "./providers";
 import { AppRoutes } from "./routes";
 
-//https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png
 function App() {
   return (
     <ApolloClientProvider>
