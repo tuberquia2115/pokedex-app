aad90fba5eb2cfa5cb0ebcece9d733b9eb751b4e
devtubercode <devtubercode@gmail.com>
2024-10-08 06:15:07 -0500

    feat: update pokemon-data and pokemon-details interfaces

--- src/interfaces/pokemon-data.interface.ts
+++ src/interfaces/pokemon-data.interface.ts
@@ -5,45 +5,4 @@ export interface PokemonData {
 export interface PokemonV2Pokemon {
   name: string;
   id: number;
-  pokemon_v2_pokemonsprites: PokemonV2Pokemonsprite[];
-}
-
-interface PokemonV2Pokemonsprite {
-  id: number;
-  sprites: Sprites;
-}
-
-interface Sprites {
-  home: Home;
-  showdown: Showdown;
-  dream_world: DreamWorld;
-  "official-artwork": OfficialArtwork;
-}
-
-interface DreamWorld {
-  front_female: null;
-  front_default: null | string;
-}
-
-interface Home {
-  front_shiny: string;
-  front_female: null | string;
-  front_default: string;
-  front_shiny_female: null | string;
-}
-
-interface OfficialArtwork {
-  front_shiny: string;
-  front_default: string;
-}
-
-interface Showdown {
-  back_shiny: string;
-  back_female: null | string;
-  front_shiny: string;
-  back_default: string;
-  front_female: null | string;
-  front_default: string;
-  back_shiny_female: null;
-  front_shiny_female: null | string;
 }
--- src/interfaces/pokemon-details.interface.ts
+++ src/interfaces/pokemon-details.interface.ts
@@ -1,3 +1,5 @@
+import { PaletteColors } from "react-palette";
+
 export interface PokemonDetailsData {
   pokemon_v2_pokemon_by_pk: PokemonV2PokemonByPk;
   pokemon_v2_generation_by_pk: PokemonV2GenerationByPkClass;
@@ -14,7 +16,6 @@ interface PokemonV2PokemonByPk {
   height: number;
   weight: number;
   pokemon_v2_pokemontypes: PokemonV2Pokemontype[];
-  pokemon_v2_pokemonsprites: PokemonV2Pokemonsprite[];
   pokemon_v2_pokemonmoves: PokemonV2Pokemonmove[];
   pokemon_v2_pokemonstats: PokemonV2Pokemonstat[];
 }
@@ -28,55 +29,36 @@ interface PokemonV2MoveClass {
   id: number;
 }
 
-interface PokemonV2Pokemonsprite {
-  sprites: Sprites;
-}
-
-interface Sprites {
-  home: Home;
-  showdown: Showdown;
-  dream_world: DreamWorld;
-  "official-artwork": OfficialArtwork;
-}
-
-interface DreamWorld {
-  front_female: null;
-  front_default: string;
-}
-
-interface Home {
-  front_shiny: string;
-  front_female: null;
-  front_default: string;
-  front_shiny_female: null;
-}
-
-interface OfficialArtwork {
-  front_shiny: string;
-  front_default: string;
-}
-
-interface Showdown {
-  back_shiny: string;
-  back_female: null;
-  front_shiny: string;
-  back_default: string;
-  front_female: null;
-  front_default: string;
-  back_shiny_female: null;
-  front_shiny_female: null;
-}
-
 interface PokemonV2Pokemonstat {
   pokemon_v2_stat: PokemonV2GenerationByPkClass;
   base_stat: number;
   stat_id: number;
 }
 
-interface PokemonV2Pokemontype {
+export interface PokemonV2Pokemontype {
   pokemon_v2_type: PokemonV2MoveClass;
 }
 
 interface PokemonV2PokemonspeciesflavortextByPk {
   flavor_text: string;
+}
+
+interface Stat {
+  name: string;
+  base_stat: number;
+  stat_id: number;
+}
+
+export interface PokemonDetails {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  picture: string;
+  pokemonColors: PaletteColors;
+  moves: string[];
+  stats: Stat[] | undefined;
+  types: PokemonV2Pokemontype[];
+  flavorText: string;
+  generationName: string;
 }
