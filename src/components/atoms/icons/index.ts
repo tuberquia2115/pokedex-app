a2f750ec6f30725bb3f063be7b929ec9cda6f860
devtubercode <devtubercode@gmail.com>
2024-10-08 10:53:51 -0500

    feat: create app icons

--- src/components/atoms/icons/index.ts
+++ src/components/atoms/icons/index.ts
@@ -2,3 +2,7 @@ export * from "./measuring-ruler-icon";
 export * from "./arrow-back-icon";
 export * from "./poke-ball-icon";
 export * from "./weight-icon";
+export * from "./close-icon";
+export * from "./search-icon";
+export * from "./favorite-icon";
+export * from "./favorite-border-icon";
