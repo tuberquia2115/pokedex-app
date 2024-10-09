b46365c22a9f1a4244263421f3c5aa1de9137ec9
devtubercode <devtubercode@gmail.com>
2024-10-09 00:12:27 -0500

    feat: update pokemon information format logic

--- src/hooks/usePokemonDetails.ts
+++ src/hooks/usePokemonDetails.ts
@@ -5,16 +5,14 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { GET_POKEMON_BY_ID } from "@/graphql";
 import { PokemonDetailsData } from "@/interfaces";
-import { getStatName, getImagePokemon } from "@/utils";
+import { getStatName, assetStorage } from "@/utils";
 import { setPokemonDetails, useAppDispatch } from "@/store";
 
 export const usePokemonDetails = () => {
   const dispatch = useAppDispatch();
   const params = useParams();
   const pokemonId = params.pokemonId;
-
-  const pokemonImg = getImagePokemon(Number(pokemonId));
-  const { data: colorsImg } = usePalette(pokemonImg);
+  const fallbackImagePokemon = assetStorage.default.fallbackImagePokemon;
 
   const queryVariables = { variables: { id: pokemonId } };
 
@@ -23,13 +21,28 @@ export const usePokemonDetails = () => {
     queryVariables
   );
 
+  const pokemonImg = useMemo(() => {
+    if (!data) return fallbackImagePokemon;
+    if (!data.pokemon_v2_pokemon_by_pk) return fallbackImagePokemon;
+
+    return data.pokemon_v2_pokemon_by_pk.pokemon_v2_pokemonsprites[0].sprites[
+      "official-artwork"
+    ].front_default;
+  }, [data, fallbackImagePokemon]);
+
+  const { data: colorsImg } = usePalette(pokemonImg);
+
   const pokemon = useMemo(() => {
     if (!data) return null;
+    if (
+      !data.pokemon_v2_pokemon_by_pk &&
+      !data.pokemon_v2_generation_by_pk &&
+      !data.pokemon_v2_pokemonspeciesflavortext_by_pk
+    )
+      return null;
+
     return {
-      picture:
-        data.pokemon_v2_pokemon_by_pk.pokemon_v2_pokemonsprites[0].sprites[
-          "official-artwork"
-        ].front_default,
+      picture: pokemonImg,
       pokemonColors: colorsImg,
       id: data?.pokemon_v2_pokemon_by_pk?.id,
       name: data?.pokemon_v2_pokemon_by_pk?.name,
@@ -49,7 +62,7 @@ export const usePokemonDetails = () => {
       types: data?.pokemon_v2_pokemon_by_pk?.pokemon_v2_pokemontypes,
       flavorText: data?.pokemon_v2_pokemonspeciesflavortext_by_pk?.flavor_text,
     };
-  }, [colorsImg, data]);
+  }, [colorsImg, data, pokemonImg]);
 
   const onSetPokemonDetails = () => {
     if (pokemon) {
