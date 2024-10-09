ff9c75a7b6c8acd9ee3c9b3950516c069590124e
devtubercode <devtubercode@gmail.com>
2024-10-09 00:12:56 -0500

    feat: remove unused function

--- src/utils/get-image-pokemon.ts
+++
@@ -1,3 +0,0 @@
-export const getImagePokemon = (pokemonId: number) => {
-  return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png`;
-};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,2 @@
-export * from "./get-image-pokemon";
 export * from "./get-stat-name";
 export * from "./asset-storage";
