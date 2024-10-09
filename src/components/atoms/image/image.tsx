d304bd90f45d0073f30bf7b7c8887050b71a6948
devtubercode <devtubercode@gmail.com>
2024-10-08 22:39:49 -0500

    feat: add component silhouette image

--- src/components/atoms/image/image.tsx
+++ src/components/atoms/image/image.tsx
@@ -1,6 +1,36 @@
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-import React, { ImgHTMLAttributes } from "react";
+import React, { ImgHTMLAttributes, useState } from "react";
 
-interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {}
+import { assetStorage } from "@/utils";
 
-export const Image: React.FC<ImageProps> = ({ ...props }) => <img {...props} />;
+import styles from "./image.module.css";
+interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+  fallbackImage?: string;
+}
+
+export const Image: React.FC<ImageProps> = ({
+  fallbackImage = assetStorage.default.fallbackImagePokemon,
+  src,
+  className,
+  ...props
+}) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [imageSrc, setImageSrc] = useState(fallbackImage);
+
+  const handleImageLoad = () => {
+    setIsLoaded(true);
+    setImageSrc(src ?? "");
+  };
+
+  const handleError = () => setImageSrc(fallbackImage);
+
+  return (
+    <img
+      className={`${styles.image} ${className}`}
+      src={imageSrc}
+      onLoad={handleImageLoad}
+      onError={handleError}
+      style={{ opacity: isLoaded ? 1 : 0.2 }}
+      {...props}
+    />
+  );
+};
